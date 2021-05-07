b047c2f55be0a8edea09e9848656cb182f1f86f3
Edvard <elsberg1995@protonmail.com>
2021-05-07 08:56:57 +0200

    binb

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function click(){
-    document.getElementById('tekst').innerHTML = 'hello';
+
+
+function clickTekstAdd(){
+    document.getElementById('tekst').innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus commodo aliquet vehicula. Suspendisse felis lacus, pharetra id nisl sollicitudin, vulputate tristique purus. Mauris tempus ante quis porta tristique. Phasellus ultrices cursus tincidunt. Aenean feugiat in elit in euismod. Curabitur nec ante felis. Donec porta lacus at augue vulputate, eget pharetra libero accumsan. Curabitur ut leo aliquam, ultrices velit in, euismod augue. Curabitur vel nulla lacus. Quisque malesuada justo urna, posuere egestas enim scelerisque eu. Morbi eget mollis purus. Nunc consequat metus vehicula mauris consequat, in tincidunt neque tristique. Sed efficitur tristique placerat. Mauris cursus lacus quis nisl malesuada, sit amet vulputate velit rhoncus. Cras vel felis sed ipsum gravida porta. Aenean euismod ut ex id tristique. In ornare congue porta. Pellentesque vehicula, nisi ac consectetur porttitor, dolor velit laoreet odio, sit amet venenatis velit felis non neque. Quisque ut justo eu metus vulputate blandit. Nulla nec eros et ligula commodo venenatis. Sed in metus a odio fringilla eleifend nec eget dolor.';
+}
+
+function clickTekstRemove(){
+    document.getElementById('tekst').innerHTML = ''
 }
