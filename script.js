fbca20773e4a70bd7af80902596b146a6bdeafd2
Edvard <elsberg1995@protonmail.com>
2021-05-07 10:55:41 +0200

    xd

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ document.getElementById("b").innerHTML = `
         <div class=content>
             <h1 id="h1">Velkommen til min portefølje!</h1>
             <img id="monkey" src="monkey.jpg" onclick="clickMonkey()">
-            <p id="tekst">Selfie af mig :) Klik mig?</p>
+            <p id="tekst">Selfie :) Klik mig?</p>
         </div>
 `
 }
-
 
 function clickMonkey(){
     document.title = 'Hey m8';
@@ -15,18 +14,20 @@ function clickMonkey(){
     document.getElementById('tekst').innerHTML =`
 
 <ul class=navbar>
-    <li style="border-left: 1px solid black;"><a href="index.html">Home</a></li>
+    <li style="border-left: 1px solid black;"><p onclick="l()">Homee</p></li>
     <li><a href="projects.html">Projects</a></li>
     <li><a href="https://www.github.com/edvardxyz">Github</a></li>
     <li><a href="contact.html">Contact</a></li>
 </ul>
 
+<p id= "main">
 
     Mauris tempus ante quis porta tristique. Phasellus ultrices cursus tincidunt. Aenean feugiat in elit in euismod. Curabitur nec ante felis. Donec porta lacus at augue vulputate, eget pharetra libero accumsan.
     Curabitur ut leo aliquam, ultrices velit in, euismod augue. Curabitur vel nulla lacus. Quisque malesuada justo urna, posuere egestas enim scelerisque eu. Morbi eget mollis purus.
     Pellentesque vehicula, nisi ac consectetur porttitor, dolor velit laoreet odio, sit amet venenatis velit felis non neque. Quisque ut justo eu metus vulputate blandit.
     Nulla nec eros et ligula commodo venenatis. Sed in metus a odio fringilla eleifend nec eget dolor.
 
+</p>
 
 
 
